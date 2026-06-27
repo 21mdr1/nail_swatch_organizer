2476fb862d7775de5efaaa7235a49ed248d2bca8
Maria Rodriguez Hertz <marurh20@gmail.com>
2026-06-27 11:42:39 -0400

    Style file picker

--- src/fragments/FilePicker.jsx
+++ src/fragments/FilePicker.jsx
@@ -4,16 +4,25 @@ import { FileUpload, Link, Image, View } from 'reshaped';
 export default function FilePicker({ file, onChange }) {
 
   return(
-    <View paddingTop={5} paddingBottom={5}>
-    <FileUpload name="file" accept="image/jpg image/png image/webp" multiple={false} onChange={onChange} >
-      Drop files to attach or{" "}
-      <FileUpload.Trigger accept="image/jpg image/png image/webp" multiple={false}>
-        <Link variant="plain">browse</Link>
-      </FileUpload.Trigger>
-    </FileUpload>
-    { file != "" && 
-      <Image src={ Array.isArray(file) ? URL.createObjectURL(file[0]) : file }/>
-    }
-    </View>
+    <div 
+      style = {{
+        marginTop: 17,
+        marginBottom: 15,
+        paddingTop: 75,
+        paddingBottom: 75,
+        paddingRight: 25,
+        paddingLeft: 25,
+        backgroundImage: file.length !== 0 ? `url(${Array.isArray(file) ? URL.createObjectURL(file[0]) : file})` : '',
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <FileUpload className='file-picker' name="file" accept="image/*" multiple={false} onChange={onChange} >
+        Drop files to attach or{" "}
+        <FileUpload.Trigger accept="image/*" multiple={false}>
+          <Link variant="plain">browse</Link>
+        </FileUpload.Trigger>
+      </FileUpload>
+    </div>
   );
 }
