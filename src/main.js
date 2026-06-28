45fd129ae215037f94ed843d937e6320818ea1c9
Maria Rodriguez Hertz <marurh20@gmail.com>
2026-06-28 08:46:32 -0400

    fix bugs with packaging

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
+if (require('electron-squirrel-startup')) app.quit();
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
